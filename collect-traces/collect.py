2ebf2ebc6615a2502cda3a205cc1440caf3f8eb9
pulls <tobias.pulls@kau.se>
2019-08-10 23:40:43 +0200

    create tmp copies of a TB directory, cleanup, proper warmup (for getting fresh consensus), and setting for timeout

--- collect-traces/collect.py
+++ collect-traces/collect.py
@@ -9,10 +9,13 @@ by calling the provided shellscript.
 """
 import argparse
 import csv
+import os
 import time
 import random
+import shutil
+import string
 import subprocess
-import os
+import tempfile
 import threading
 
 ap = argparse.ArgumentParser()
@@ -20,10 +23,20 @@ ap.add_argument("-l", required=True,
     help="Alexa file with list of sites to visit")
 ap.add_argument("-n", required=True, type=int,
     help="number of samples")
+
+# all data is stored in the below dir and checks are made against the folder if
+# data has already been collected before collecting again, multiple instances of
+# this script are assumed to possibly be using the same shared directory
 ap.add_argument("-d", required=True,
-    help="shared data folder for storing results")
+    help="(shared) data folder for storing results")
+
+# make sure that the IP address of the guard is that which is hardcoded as the
+# guard in Browser/TorBrowser/Data/Tor/torrc (using EntryNodes fingerprint) of
+# the instance of Tor Browser
 ap.add_argument("-g", required=True, 
     help="IP address of guard")
+ap.add_argument("-b", required=True, 
+    help="folder with Tor Browser")
 
 ap.add_argument("-a", required=False, default=5, type=int,
     help="number of attempts to collect each trace")
@@ -31,20 +44,32 @@ ap.add_argument("-t", required=False, default=60, type=int,
     help="timeout (s) for each TB visit")
 ap.add_argument("-i", required=False, default="eth0",
     help="network interface to capture from")
+
+# SnapLen of 68 bytes for IPv4 packets and 96 bytes for IPv6 packets
 ap.add_argument("-s", required=False, default=68, type=int,
     help="snaplen to capture")
+
 # with default snaplen 68, expect at least 100 packets, and account for pcap
 # header of 24 bytes: 68x100+24 = 6824
-ap.add_argument("-m", required=False, default=1384, type=int,
+ap.add_argument("-m", required=False, default=6824, type=int,
     help="minimum pcap size to accept (bytes)")
 args = vars(ap.parse_args())
 
 RESULTSFMT = "{}-{}.pcap"
 TSHARKFMT = "tshark -i {} -f \"host {} and length > 511\" -s {} -a duration:{} -w {} -F libpcap"
+TBFILE = "start-tor-browser.desktop"
+
+tmpdir = tempfile.mkdtemp()
 
 def main():
     if not os.path.exists(args["d"]):
         print("data directory {} does not exist".format(args["d"]))
+        return -1
+    if not os.path.exists(args["b"]):
+        print("Tor Browser directory {} does not exist".format(args["b"]))
+        return -1
+    if not os.path.isfile(os.path.join(args["b"], TBFILE)):
+        print("Tor Browser directory {} missing {}".format(args["b"], TBFILE))
         return -1
 
     print("reading Alexa site list {}".format(args["l"]))
@@ -52,6 +77,9 @@ def main():
     print("ok, list has {} sites".format(len(alexa)))
 
     # FIXME: make a copy of TB, warmup visits, make a copy again and go
+    tb = make_tb_copy(args["b"])
+    visit("kau.se/en", tb, args["t"])
+    visit("kau.se/en/cs", tb, args["t"])
 
     # random order of samples and alexa, ensuring that each instance of this
     # script won't be working on the same alexa-sample pair
@@ -62,20 +90,22 @@ def main():
         for index, site in alexa:
             # only attempt to collect if not already collected
             if not os.path.isfile(results_file(index, n)):
-                collect(index, site, n)
+                collect(index, site, n, tb)
 
-    # FIXME: cleanup copies of TB
+    # remove tmpdir
+    shutil.rmtree(tmpdir)
 
-def collect(index, site, sample):
+def collect(index, site, sample, tb_orig):
     print("collect index {}, site {}, sample {}".format(index, site, sample))
     fname = results_file(index, sample)
 
-    # FIXME: make TB copy
+    # make a dedicated TB copy for this website
+    tb = make_tb_copy(tb_orig)
 
     for a in range(args["a"]):
         # after two attempts, try to toggle www. prefix on url
         if a >= 2:
-            site = togglewww(site)
+            site = toggle_www(site)
         
         # start network capture in new thread
         t = threading.Thread(target=capture, args=(fname,))
@@ -85,8 +115,7 @@ def collect(index, site, sample):
         time.sleep(1)
 
         # visit with TB, blocking
-        #subprocess.call("./visit.sh {} {}".format(site, sample), shell=True)
-        visit(site)
+        visit(site, tb, args["t"]-1)
 
         # wait for network capture to finish
         t.join()
@@ -94,16 +123,28 @@ def collect(index, site, sample):
         # if capture was successful and at least of minimum size, break
         if os.path.isfile(fname) and os.path.getsize(fname) >= args["m"]:
             break
+    
+    cleanup_tb_copy(tb)
 
-def visit(url):
-    subprocess.call("./visit.sh {}".format(url), shell=True)
+def make_tb_copy(src):
+    dst = os.path.join(tmpdir, 
+    ''.join(random.choices(string.ascii_uppercase + string.digits, k=24)))
+    shutil.copytree(src, dst)
+    return dst
+
+def cleanup_tb_copy(c):
+    return shutil.rmtree(c)
+
+def visit(url, tb, timeout):
+    tb = os.path.join(tb, "Browser", "start-tor-browser")
+    subprocess.call("./visit.sh {} {} {}".format(url, tb, timeout), shell=True)
 
 def capture(fname):
     cmd = TSHARKFMT.format(args["i"], args["g"], args["s"], args["t"], fname)
     print(cmd)
     subprocess.call(cmd, shell=True)
 
-def togglewww(site):
+def toggle_www(site):
     if site.startswith("www."):
         return site[4:]
     else:
--- collect-traces/visit.sh
+++ collect-traces/visit.sh
@@ -1,1 +1,2 @@
-echo "FIXME, visit $1 with TB"
+echo "visiting $1 with TB at $2, timeout $3"
+timeout $3 $2 --headless $1
