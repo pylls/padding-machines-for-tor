77608b3f63f1f5728da9171a5222eb2f24a0f49b
pulls <tobias.pulls@kau.se>
2019-08-14 17:23:13 +0200

    outline

---
+++ pcap-to-cells/pcap-to-cells.py
@@ -0,0 +1,12 @@
+#!/usr/bin/env python3
+
+def main():
+    print("hello world")
+
+def parse():
+    # loop
+    # count size of burst (TCP length)
+    # divide burst size by constant (512 or 514)
+
+if __name__ == "__main__":
+    main()
